8927e57e089210dca9228dcdfd2f395dda6fb0bc
maliha shahi <maliha.shahi.03@gmail.com>
2024-05-24 23:13:54 +0430

    python datatype

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
-print("Hello world")
+print(type('MAliha'))   # this is an string
+
+print('--------------------')
+
+print(type(180))  # this is an integere
+
+print('------------------------')
+
+print(type(75.5)) # this is a float
+
+print('---------------------')
+
+
+# this is a complex
+print(type(65j))  # this is a complex
+
+print('Im Maliha')
+print('Im from Afghanistan')
