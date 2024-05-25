6615d1ccdf2bbbec4898c2ffd55ade64bec5008f
maliha shahi <maliha.shahi.03@gmail.com>
2024-05-25 22:18:41 +0430

    casting in python

--- first.py
+++ first.py
@@ -1,15 +1,3 @@
-# print(4 + 2)
-# print(10 - 3 - 4 - 15)
-# print(15 + 5 - 6)
-# print(4 * 2 * 5)
-# print(12 / 3)
-
-
-# print(1 + 3 * 4 / 2)
-# print((1 + 3) * 4 / 2)
-
-print(8 % 2)
-print(8 % 3)
-
-print(4 ** 2)
-print(type(8 // 3))
+x=int (3)
+y=int(2.3)
+print(type(y))
