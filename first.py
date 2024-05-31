6a5f670bfb8822457527914efe32919125335712
maliha shahi <maliha.shahi.03@gmail.com>
2024-05-31 22:50:48 +0430

    list and len function python

--- first.py
+++ first.py
@@ -1,13 +1,4 @@
-
-first_number= input("choose a number 1-9 :")
-first_number =int(first_number)
-number=first_number
-
-number*= 2
-number +=8
-number += first_number
-number -=2
-number /=3
-number -=first_number
-number*=4
-print(number)
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
