ef551c95e8c4665c73342032f35138f33e0b2da2
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-01 22:48:08 +0430

    change list items ,add list item with insert and append metod

--- first.py
+++ first.py
@@ -1,6 +1,11 @@
-mylist = ["apple", "banana", "cherry"]
-#print(type(mylist))
-print(mylist[2:3])
-print(type(mylist[2:3]))
-print(mylist[:2])
-print(mylist[0:])
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+#.//////////////////////////////////////
+thislist1 = ["apple", "banana", "cherry"]
+thislist1.append("orange")
+print(thislist1)
+#/////////////////////////////////////////
+thislist2 = ["apple", "banana", "cherry"]
+thislist2.insert(1, "orange")
+print(thislist2)
