fabda3379362f164a5f9cc847e71b3266533028a
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-03 22:11:01 +0430

    join two list with these method + ,append, extend

--- first.py
+++ first.py
@@ -1,16 +1,19 @@
-thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist.sort()
-print(thislist)
-#///////////////////////////////////////
-thislist2 = [100, 50, 65, 82, 23]
-thislist2.sort()
-print(thislist2)
-#/////////////
-thislist3 = ["orange", "mango", "kiwi", "pineapple", "banana"]
-thislist3.sort(reverse = True)
-print(thislist3)
-#///////////////////////////////////////////////////////////
-thislist4 = ["banana", "Orange", "Kiwi", "cherry"]
-thislist4.reverse()
-print(thislist4)
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
 
+list3 = list1 + list2
+print(list3)
+#/////////////////////////////////
+list3 = ["a", "b" , "c"]
+list4 = [1, 2, 3]
+
+for x in list4:
+  list3.append(x)
+
+print(list3)
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+list5 = ["a", "b" , "c"]
+list6 = [1, 2, 3]
+
+list5.extend(list6)
+print(list5)
