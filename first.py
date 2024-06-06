7d0974e6ed8205190b58a8be8dafd2dc7c8b893f
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-06 22:54:55 +0430

    dictionary find the lenth ,Type also din allow any data type

--- first.py
+++ first.py
@@ -1,28 +1,17 @@
-#...................union ..............
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
-
-myset = set1.union(set2, set3, set4)
-print(myset)
-#//////////////join set with tuple///////////////////////////////
-x = {"a", "b", "c"}
-y = (1, 2, 3)
-
-z = x.union(y)
-print(z)
-#/////////////////intersection &//////////////////////////////////\
-set5 = {"apple", "banana", "cherry"}
-set6 = {"google", "microsoft", "apple"}
-
-set7 = set1.intersection(set6)
-print(set7)
-
-#........../////////difference - ///////////////////..................
-set8 = {"apple", "banana", "cherry"}
-set9 = {"google", "microsoft", "apple"}
-
-set10= set8.difference(set9)
-
-print(set10)
+#///////////////////////////////dictionary/////////////////
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+#;;;;;;;;;;;;;;;;;;;find dictionary lenth;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+print(len(thisdict))
+#/////////////dictionary allow  any data type/////////////////////
+thisdict2 = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict2)
