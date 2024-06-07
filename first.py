cb0fddf6de1b70bba4724e70097a5e827a1ae4c6
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-07 22:50:37 +0430

    update and add items on dictionary

--- first.py
+++ first.py
@@ -1,11 +1,30 @@
+
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-x = thisdict["model"]
-#;;;;;;;;;;;;;;;Access with get method;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-x1 = thisdict.get("model")
-#////////////////// keys method///////////////////////////
-x2 = thisdict.keys()
-#........................................................
+thisdict["year"] = 2018
+#;;;;;;;;;;;;;;;;;;; use update method;;;;;;;;;;;;;;;;;;;;;;;;;
+thisdict1 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}  
+thisdict1.update({"year": 2020})
+#lllllllllllllllllllllllllllllllladd item to dictionary  
+
+thisdict2 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict2["color"] = "red"
+print(thisdict2)
+#.......................use update to add item...................................
+thisdict3 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict3.update({"color": "red"})
