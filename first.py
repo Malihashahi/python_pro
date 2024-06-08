a8671868921da90b318ff5b34c4a269e0ee7f0c6
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-08 22:55:43 +0430

    copy items on dictionary and nested dictionary

--- first.py
+++ first.py
@@ -1,32 +1,23 @@
-#..........................................use pop .............
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-thisdict.pop("model")
-print(thisdict)
-#,,,,,,,,,,.....pop item method....................
-thisdict1 = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+mydict = thisdict.copy()
+print(mydict)
+
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,/
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
 }
-thisdict1.popitem()
-print(thisdict1)
-#..................del method usage...........................
-thisdict2 = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-del thisdict2["model"]
-print(thisdict2)
-#.......................clear method................................
-thisdict3 = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict3.clear()
-print(thisdict3)
