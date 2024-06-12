237ebdbeac1c6954d3e199ad6d9ea65f0673daf8
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-12 22:30:58 +0430

    create simple login and check in keyword

--- first.py
+++ first.py
@@ -1,12 +1,13 @@
-i = 1
-while i < 6:
-  print(i)
-  if i == 3:
-    break
+users ={
+  "bahar":"1234",
+  "maliha": "6854",
+  "amir": "35333"
+}
 
-  k = 0
-while k< 6:
-  k += 1
-  if i == 3:
-    continue
-  print(k)
+
+entered_username = input('enter your username :')
+entered_password = input('enter your password :')
+if entered_username in users:
+  print('yes your welcome')
+else:
+  print('your not our users')
