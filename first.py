b09c5366c453096c4e7ea82be83d7b665aed3385
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-13 22:44:08 +0430

    for loop use

--- first.py
+++ first.py
@@ -1,13 +1,5 @@
-users ={
-  "bahar":"1234",
-  "maliha": "6854",
-  "amir": "35333"
-}
-
-
-entered_username = input('enter your username :')
-entered_password = input('enter your password :')
-while entered_username not in users or users[entered_username] != entered_password:
-  print('your username is worng')
-  entered_username= input('enter your username again : ')
-  entered_password= input('enter your password again')
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
