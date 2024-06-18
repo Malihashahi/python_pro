a54c2ef45fcb2fcc9bf77ed5cd2a15a053dc3d1c
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-18 21:50:25 +0430

    funtion in python

--- first.py
+++ first.py
@@ -1,8 +1,4 @@
-name = input('enter your name : ')
-name=name.lower()
-name=name.replace(" ","")
-b= []
-for  n in name:
-  if n not in b:
-    print(f"your name has {name.count(n)} {n}")
-    b.append(n)
+def my_function():
+  print("Hello from a function")
+
+my_function()
