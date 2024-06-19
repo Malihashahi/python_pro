029dd787fe7a680671909a99381b4ceebd447d78
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-19 22:55:29 +0430

    create password validation function

--- first.py
+++ first.py
@@ -1,4 +1,14 @@
-def my_function():
-  print("Hello from a function")
+def passvalidation(password):
+  if len(password)< 8:
+     print("your password must be at least 8 character ")
+  elif password.isnumeric():
+     print("your password must have at least one letter ")
+  elif password.isalpha():
+     print("your password must have at least one number ")
+  else:
+     print("yes your password is correct ")
 
-my_function()
+
+while True:
+  password = input('enter your password : ')
+  passvalidation(password)
