47e28e692b0520730f325a74233baf672f3c3e79
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-20 22:46:04 +0430

    *args in functions

--- first.py
+++ first.py
@@ -1,14 +1,4 @@
-def passvalidation(password):
-  if len(password)< 8:
-     print("your password must be at least 8 character ")
-  elif password.isnumeric():
-     print("your password must have at least one letter ")
-  elif password.isalpha():
-     print("your password must have at least one number ")
-  else:
-     print("yes your password is correct ")
+def my_function(*args):
+  print("The youngest child is " + args[2])
 
-
-while True:
-  password = input('enter your password : ')
-  passvalidation(password)
+my_function("Emil", "Tobias", "Linus")
