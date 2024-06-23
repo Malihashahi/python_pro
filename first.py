59e374798e1e421b33870eabff678dd699648175
maliha shahi <maliha.shahi.03@gmail.com>
2024-06-23 22:25:09 +0430

    function  that show upper and lower cases

--- first.py
+++ first.py
@@ -1,6 +1,19 @@
-def my_function(x):
-  return 5 * x
 
-print(my_function(3))
-print(my_function(5))
-print(my_function(9))
+def myfunc(name):
+  ups=0
+  lows=0
+  for n in name:
+    if n.isupper():
+         ups += 1
+    elif  n.islower():
+         lows += 1
+    else: 
+         pass
+       
+  print(f"upper cases: {ups}")
+  print(f"lower cases: {lows}")
+
+
+
+name = input('enter your name : ')
+myfunc(name)
