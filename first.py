5395def92b82cb6f839e797fc7f4b37ac8f89a96
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-07 21:43:45 +0430

    scop at python

--- first.py
+++ first.py
@@ -1,21 +1,5 @@
-class User:
-    def __init__(self ,firstname ,lastname):
-      self.fname = firstname
-      self.lname = lastname
-    def fullname(self):
-       print(f"my fullname is {self.fname}{self.lname}")
-  
+def myfunc():
+  global x
+  x = 300
 
-class Student(User):
-    def __init__(self, firstname, lastname ,email):
-       super().__init__(firstname, lastname)
-       self.email =email
-    
-    def fullname(self):
-       print("I am a student")
-       return super().fullname()
-    
-
-    
-s1 = Student("Maliha " ,"Shahi" ,"maliha@gmail.com")
-s1.fullname()
+myfunc()
