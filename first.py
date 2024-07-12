5c09ad6e358e70044fb17a115b0c6aceb013e614
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-12 11:14:43 +0430

    math funthion in python lik : min, max, ceil, floor

--- first.py
+++ first.py
@@ -1,5 +1,15 @@
-import datetime
+import math
 
-x = datetime.datetime(2018, 6, 1)
+x = math.ceil(1.4)
+y = math.floor(1.4)
 
-print(x.strftime("%B"))
+print(x) # returns 2
+print(y) # returns 1
+
+
+
+mylist = [16,34,67]
+b= max(mylist)
+k= min(mylist)
+print('max is ',b)
+print('min is ',k)
