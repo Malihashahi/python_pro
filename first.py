4231ca7dc2e8c7e03ec53d211cead13d215e0877
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-16 22:48:24 +0430

    cowsay in python

--- first.py
+++ first.py
@@ -1,6 +1,5 @@
-import re
+import cowsay
 
-txt = "The rain in Spain"
-x = re.search("\s", txt)
 
-print("The first white-space character is located in position:", x.start())
+
+cowsay.cow('Hello World')
