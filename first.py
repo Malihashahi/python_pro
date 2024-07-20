bf2be7d1da5fddc5151b4142d19adda69920831a
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-20 21:54:21 +0430

    excaption hadeling with try and except

--- first.py
+++ first.py
@@ -1,5 +1,10 @@
-import cowsay
-
-
-
-cowsay.cow('Hello World')
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
