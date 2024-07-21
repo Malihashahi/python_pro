6af2c562e6a87a39dced23fe10b07a5d1243ce45
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-21 22:04:18 +0430

    String Formatting in python

--- first.py
+++ first.py
@@ -1,10 +1,3 @@
-try:
-  f = open("demofile.txt")
-  try:
-    f.write("Lorum Ipsum")
-  except:
-    print("Something went wrong when writing to the file")
-  finally:
-    f.close()
-except:
-  print("Something went wrong when opening the file")
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
