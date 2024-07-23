b7e0a631aa6633ac9f38daa88c6c9aa03f16384c
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-23 21:44:26 +0430

    iteration with an example  stop iteration

--- first.py
+++ first.py
@@ -1,8 +1,18 @@
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = []
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
 
-for x in fruits:
-  if "a" in x:
-    newlist.append(x)
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
 
-print(newlist)
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
