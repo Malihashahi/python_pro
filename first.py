2034f56a4fb24e95ddc55e4d98fe36c66814f8c9
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-24 22:53:42 +0430

    decorator funtion with example

--- first.py
+++ first.py
@@ -1,18 +1,32 @@
-class MyNumbers:
-  def __iter__(self):
-    self.a = 1
-    return self
+# defining a decorator
+def hello_decorator(func):
 
-  def __next__(self):
-    if self.a <= 20:
-      x = self.a
-      self.a += 1
-      return x
-    else:
-      raise StopIteration
+    # inner1 is a Wrapper function in 
+    # which the argument is called
+    
+    # inner function can access the outer local
+    # functions like in this case "func"
+    def inner1():
+        print("Hello, this is before function execution")
 
-myclass = MyNumbers()
-myiter = iter(myclass)
+        # calling the actual function now
+        # inside the wrapper function.
+        func()
 
-for x in myiter:
-  print(x)
+        print("This is after function execution")
+        
+    return inner1
+
+
+# defining a function, to be called inside wrapper
+def function_to_be_used():
+    print("This is inside the function !!")
+
+
+# passing 'function_to_be_used' inside the
+# decorator to control its behaviour
+function_to_be_used = hello_decorator(function_to_be_used)
+
+
+# calling the function
+function_to_be_used()
