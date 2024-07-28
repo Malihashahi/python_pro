c6a7206b7124e5873502cb348f2f935ad28b23e5
maliha shahi <maliha.shahi.03@gmail.com>
2024-07-28 23:22:56 +0430

    getter and setter method

--- first.py
+++ first.py
@@ -1,27 +1,25 @@
-class Base:
-    def __init__(self):
-        self.a = "mlih"
-        self.__c = "mslin"
-
-# Creating a derived class
-class Derived(Base):
-    def __init__(self):
-
-        # Calling constructor of
-        # Base class
-        Base.__init__(self)
-        print("Calling private member of base class: ")
-        print(self.__c)
-
-
-# Driver code
-obj1 = Base()
-print(obj1.a)
-
-# Uncommenting print(obj1.c) will
-# raise an AttributeError
-
-# Uncommenting obj2 = Derived() will
-# also raise an AttributeError as
-# private member of base class
-# is called inside derived class
+# Python program showing a use 
+# of get() and set() method in 
+# normal function 
+  
+class Geek: 
+    def __init__(self, age = 0): 
+         self._age = age 
+      
+    # getter method 
+    def get_age(self): 
+        return self._age 
+      
+    # setter method 
+    def set_age(self, x): 
+        self._age = x 
+  
+raj = Geek() 
+  
+# setting the age using setter 
+raj.set_age(21) 
+  
+# retrieving age using getter 
+print(raj.get_age()) 
+  
+print(raj._age) 
