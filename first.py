07a26dcaf892e8ad604c7281a132316f43b44459
maliha shahi <maliha.shahi.03@gmail.com>
2024-08-02 23:02:23 +0430

    generator  with yield key

--- first.py
+++ first.py
@@ -1,28 +1,10 @@
+def gen_with_error():
+    yield 1
+    raise ValueError("Error inside generator")
 
-
-# Python program to demonstrate writing of __repr__ and
-# __str__ for user defined classes
- 
-# A user defined class to represent Complex numbers
-class Complex:
- 
-    # Constructor
-    def __init__(self, real, imag):
-     self.real = real
-     self.imag = imag
- 
-    # For call to repr(). Prints object's information
-    def __repr__(self):
-     return 'Rational(%s, %s)' % (self.real, self.imag)    
- 
-    # For call to str(). Prints readable form
-    def __str__(self):
-     return '%s + i%s' % (self.real, self.imag)    
- 
- 
-# Driver program to test above
-t = Complex(10, 20)
- 
-# Same as "print t"
-print (str(t))
-print (repr(t))
+g = gen_with_error()
+print(next(g))
+try:
+    g.throw(ValueError, "Error thrown")
+except ValueError as e:
+    print(e)
